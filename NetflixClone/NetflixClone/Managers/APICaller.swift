cb0f25d26a725939a14159e9289177cd989e9dbe
brightsummit.co <naseemoyebola@gmail.com>
2022-03-17 11:29:44 +0100

    fetch searchData

--- NetflixClone/NetflixClone/Managers/APICaller.swift
+++ NetflixClone/NetflixClone/Managers/APICaller.swift
@@ -123,4 +123,22 @@ class APICaller {
     task.resume()
   }
   
+  func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void) {
+    guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+    
+    guard let url = URL(string: "\(Endpoint.baseURL)/3/search/movie?api_key=\(Endpoint.API_KEY)&query=\(query)") else { return }
+    let task = URLSession.shared.dataTask(with: url) { data, _, error in
+      guard let data = data, error == nil else {
+        return
+      }
+      do {
+        let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+        completion(.success(results.results))
+      } catch {
+        completion(.failure(APIError.failedToGetData))
+      }
+    }
+    task.resume()
+  }
+  
 }
