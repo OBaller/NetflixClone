9d259c7cd6060f9ce3ff26cd6dc9e9337856119b
brightsummit.co <naseemoyebola@gmail.com>
2022-03-19 13:13:19 +0100

    feat: Delete downloads

--- NetflixClone/NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -56,7 +56,7 @@ class CollectionViewTableViewCell: UITableViewCell {
     DataPersistenceManager.shared.downloadWith(model: titles[indexPath.row]) { result in
       switch result {
         case .success():
-          print("Downloaded to database")
+          NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
         case .failure(let error):
           print(error.localizedDescription)
       }
